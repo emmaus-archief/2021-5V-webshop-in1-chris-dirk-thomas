76a89b339559a27c2d2b1566e0e4776141dfbff7
zWolv <samoht684@gmail.com>
2020-11-26 09:05:21 +0000

    beginnetje

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,12 @@
-DROP TABLE IF EXISTS products; 
-CREATE TABLE products (
+DROP TABLE IF EXISTS hengels; 
+CREATE TABLE hengels (
   ID SERIAL PRIMARY KEY,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  lengte INTEGER,
+  max_gewicht INTEGER,
+  soort_id INTEGER
+  soortgelijke_hengel INTEGER
 );
