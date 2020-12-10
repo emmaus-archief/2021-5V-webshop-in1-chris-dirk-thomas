a0a5ea4288e52f7f332fc0e257da34286b1b265d
zWolv <samoht684@gmail.com>
2020-12-10 08:46:16 +0000

    error gefixt

--- sql/seed.sql
+++ sql/seed.sql
@@ -7,62 +7,62 @@
 -- producttype 2 = aas
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
-VALUES ("75847", "Abu Garcia Black Max Combo", "De Black Max Combo van Abu Garcia bestaat uit 
+VALUES ('75847', 'Abu Garcia Black Max Combo', 'De Black Max Combo van Abu Garcia bestaat uit 
 een pittige jerkbaithengel voor kunstaas tot ongeveer 45gram en een prima baitcast reel
  die is voorzien van 5 kogellagers en een uitstekende slip. Schitterende set voor de 
- beginnende kunstaasvisser!", 75.00, 2, 1);
-/*
-INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
-VALUES ("57694", "Abu Garcia Pro Max", "Lichtgewichte compacte combi
- die is gemaakt om dag in, dag uit alle worstelingen te kunnen weerstaan.", 94.99, 4, 1);
+ beginnende kunstaasvisser!', 75.00, 2, 1);
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
-VALUES ("98237", "NGT Drop Shot Combo", "Met deze zeer complete startersset kun je
+VALUES ('57694', 'Abu Garcia Pro Max', 'Lichtgewichte compacte combi
+ die is gemaakt om dag in, dag uit alle worstelingen te kunnen weerstaan.', 94.99, 4, 1);
+
+INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
+VALUES ('98237', 'NGT Drop Shot Combo', 'Met deze zeer complete startersset kun je
    direct aan de slag met de uitdagende dropshotvisserij. De set bestaat uit een
    NGT dropshothengel, -molen volgespoeld met lijn, kunstaas, dropshot rigs en 
    dropshot lood! De gevoelige hengel geeft elke aanbeet perfect door, terwijl
    je voldoende ruggengraat over hebt om ook grotere vis rustig uit te drillen. 
    De soepel draaiende molen is volgespoeld met topkwaliteit 10lb nylon lijn. 
    De hele set is zeer lichtgewicht, zodat je er de hele dag comfortabel mee kunt 
-   vissen!", 35.89, 1, 1);
+   vissen!', 35.89, 1, 1);
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
-VALUES ("85624", "Abu Garcia Revo X Combo", "Deze combo set is perfect voor de roofvisser
+VALUES ('85624', 'Abu Garcia Revo X Combo', 'Deze combo set is perfect voor de roofvisser
  die op zoek is naar een degelijke set. Dankzij de Carbon Matrix slip is het drillen
   van grotere roofvis geen probleem voor deze molen. De WinnGrip handvat zorgt voor
-   een prettige grip en unieke uitstraling.", 96.69, 2, 1);
+   een prettige grip en unieke uitstraling.', 96.69, 2, 1);
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype) 
-VALUES ( "37373", "Mitchell MX4 Spinning Combo H", "De MX4 Spinning Combos van Mitchell 
+VALUES ('37373', 'Mitchell MX4 Spinning Combo H', 'De MX4 Spinning Combos van Mitchell 
 zijn goed voor heel veel visplezier! Of je nou vist op baars in de rivier of op zeevis 
 vanuit een boot, er is altijd een geschikte Mitchell combo verkrijgbaar. 
 Deze Mitchell MX4 Spinning Combo is een fraaie complete allround set welke 
 te gebruiken is voor de zwaardere visserijen. 
 De set bestaat uit een 2.74 meter lange spinhengel met een bijpassende molen,
- zo kan je bijna meteen naar de waterkant!", 74.88, 3, 1);
+ zo kan je bijna meteen naar de waterkant!', 74.88, 3, 1);
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype) 
-VALUES ("64836", "Berkler Powerbait Pulse Shad 3st",
-"Door de specifieke vorm van de staart van de Pulse Shad is het mogelijk om deze 
+VALUES ('64836', 'Berkler Powerbait Pulse Shad 3st',
+'Door de specifieke vorm van de staart van de Pulse Shad is het mogelijk om deze 
 softbait zeer langzaam binne te halen zonder dat het aas zijn mooie actie verliest. 
-Vooral bij wisselend binnenhalen tussen snel en langzaam is dit een gigantisch voordeel.", 6.54, 2, 2);
+Vooral bij wisselend binnenhalen tussen snel en langzaam is dit een gigantisch voordeel.', 6.54, 2, 2);
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
-VALUES ("69420", "Berkley Powerbait Hollow Belly", "De peddelvormige staart van deze 
+VALUES ('69420', 'Berkley Powerbait Hollow Belly', 'De peddelvormige staart van deze 
 shad van Berkley zorgt voor een brede zwembeweging en maakt de roofvis helemaal wild. 
 De staart zorgt voor veel visuele aantrekkingskracht omdat het een breed spoor achterlaat. 
 Doordat de shad een holle buik heeft is het eenvoudig om de haak te zetten. Bovendien 
 zorgt het voor een levendig uiterlijk wat in combinatie met de geur van de Powerbait er 
-voor zorgt dat dit aas onweerstaanbaar is voor roofvis. Ook wanneer de vis z'n tanden in 
+voor zorgt dat dit aas onweerstaanbaar is voor roofvis. Ook wanneer de vis zijn tanden in 
 de shad zet, voelt deze veel zachter en natuurlijker aan dan een massieve shad wat het 
-verspelen van vis sterk verminderd.", 9.98, 1, 2);
+verspelen van vis sterk verminderd.', 9.98, 1, 2);
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
-VALUES ("56511", "Fox Rage Spinnerbait", "Wanneer snoek niet wilt bijten dan moet je
+VALUES ('56511', 'Fox Rage Spinnerbait', 'Wanneer snoek niet wilt bijten dan moet je
  deze spinnerbait eens proberen. Deze spinnerbait zorgt voor zo veel beweging en 
  attractie in het water dat snoek er gewoon niet omheen kan. De spinnerbait heeft 
  twee spinnerbladen en een grote bucktail met daarin een haak verstopt. 
- De ogen op de spinnerbait maken de aantrekkingskracht af.", 11.77, 4, 2);
+ De ogen op de spinnerbait maken de aantrekkingskracht af.', 11.77, 4, 2);
 
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ()
@@ -80,7 +80,7 @@ INSERT INTO hengel_eigenschappen (lengte, max_gewicht, hengelsoort_id) VALUES (2
 
 INSERT INTO hengel_eigenschappen (lengte, max_gewicht, hengelsoort_id) VALUES (274, 60, 2);
 
-INSERT INTO hengel_soort (hengelsoort) VALUES ("hengel met reel");
+INSERT INTO hengel_soort (hengelsoort) VALUES ('hengel met reel');
 
-INSERT INTO hengel_soort (hengelsoort) VALUES ("hengel met molen");
-*/
+INSERT INTO hengel_soort (hengelsoort) VALUES ('hengel met molen');
+
