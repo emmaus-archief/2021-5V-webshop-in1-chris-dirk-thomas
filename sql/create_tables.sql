9f1265b7e28ed1c594800f144c7b9ac976bf1f23
zWolv <samoht684@gmail.com>
2020-12-17 13:38:24 +0000

    comments

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,4 @@
+-- hoofdtabel met de producten -- self join met A.ook_bekeken = B.id
 DROP TABLE IF EXISTS products; 
 CREATE TABLE products (
   ID SERIAL PRIMARY KEY,
@@ -9,6 +10,7 @@ CREATE TABLE products (
   producttype INT
 );
 
+-- tabel met de eigenschappen van de hengels -- join met products als producttype 1 is op hengel_eigenschappen.id = products.id
 DROP TABLE IF EXISTS hengel_eigenschappen;
 CREATE TABLE hengel_eigenschappen (
   ID SERIAL PRIMARY KEY,
@@ -17,21 +19,25 @@ CREATE TABLE hengel_eigenschappen (
   hengelsoort_id INT
 );
 
+-- tabel met de soort hengel -- join met hengel_eigenschappen op hengel_soort.id = hengel_eigenschappen.hengelsoort_id
 DROP TABLE IF EXISTS hengel_soort;
 CREATE TABLE hengel_soort (
   ID SERIAL PRIMARY KEY,
   hengelsoort TEXT
 );
 
+-- tabel met de soort aas -- join met aas_eigenschappen op aas_eigenschappen.aassoort_id = soort_aas.id
 DROP TABLE IF EXISTS soort_aas;
 CREATE TABLE soort_aas (
   ID SERIAL PRIMARY KEY,
   aassoort TEXT
 );
 
+-- tabel met de eigenschappen van het aas -- join met products als producttype 2 is op aas_eigenschappen.id = products.id
 DROP TABLE IF EXISTS aas_eigenschappen;
 CREATE TABLE aas_eigenschappen (
   ID SERIAL PRIMARY KEY,
   gewicht INT,
-  lengte INT
+  lengte INT,
+  aassoort_id INT
 );
--- sql/seed.sql
+++ sql/seed.sql
@@ -3,20 +3,24 @@
 --
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
--- producttype 1 = hengel
--- producttype 2 = aas
--- browser fix
 
+
+
+---- producten tabel
+--- hengels
+--01
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('75847', 'Abu Garcia Black Max Combo', 'De Black Max Combo van Abu Garcia bestaat uit 
 een pittige jerkbaithengel voor kunstaas tot ongeveer 45gram en een prima baitcast reel
  die is voorzien van 5 kogellagers en een uitstekende slip. Schitterende set voor de 
  beginnende kunstaasvisser!', 75.00, 2, 1);
 
+--02
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('57694', 'Abu Garcia Pro Max', 'Lichtgewichte compacte combi
  die is gemaakt om dag in, dag uit alle worstelingen te kunnen weerstaan.', 94.99, 4, 1);
 
+--03
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('98237', 'NGT Drop Shot Combo', 'Met deze zeer complete startersset kun je
    direct aan de slag met de uitdagende dropshotvisserij. De set bestaat uit een
@@ -27,12 +31,14 @@ VALUES ('98237', 'NGT Drop Shot Combo', 'Met deze zeer complete startersset kun 
    De hele set is zeer lichtgewicht, zodat je er de hele dag comfortabel mee kunt 
    vissen!', 35.89, 1, 1);
 
+--04
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('85624', 'Abu Garcia Revo X Combo', 'Deze combo set is perfect voor de roofvisser
  die op zoek is naar een degelijke set. Dankzij de Carbon Matrix slip is het drillen
   van grotere roofvis geen probleem voor deze molen. De WinnGrip handvat zorgt voor
    een prettige grip en unieke uitstraling.', 96.69, 2, 1);
 
+--05
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype) 
 VALUES ('37373', 'Mitchell MX4 Spinning Combo H', 'De MX4 Spinning Combos van Mitchell 
 zijn goed voor heel veel visplezier! Of je nou vist op baars in de rivier of op zeevis 
@@ -42,13 +48,16 @@ te gebruiken is voor de zwaardere visserijen.
 De set bestaat uit een 2.74 meter lange spinhengel met een bijpassende molen,
  zo kan je bijna meteen naar de waterkant!', 74.88, 3, 1);
 
+--- aas
+--06
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype) 
 VALUES ('64836', 'Berkler Powerbait Pulse Shad 3st',
 'Door de specifieke vorm van de staart van de Pulse Shad is het mogelijk om deze 
 softbait zeer langzaam binne te halen zonder dat het aas zijn mooie actie verliest. 
 Vooral bij wisselend binnenhalen tussen snel en langzaam is dit
- een gigantisch voordeel.', 6.54, 10, 2); --pulse
+ een gigantisch voordeel.', 6.54, 10, 2);
 
+--07
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('69420', 'Berkley Powerbait Hollow Belly', 'De peddelvormige staart van deze 
 shad van Berkley zorgt voor een brede zwembeweging en maakt de roofvis helemaal wild. 
@@ -57,23 +66,26 @@ Doordat de shad een holle buik heeft is het eenvoudig om de haak te zetten. Bove
 zorgt het voor een levendig uiterlijk wat in combinatie met de geur van de Powerbait er 
 voor zorgt dat dit aas onweerstaanbaar is voor roofvis. Ook wanneer de vis zijn tanden in 
 de shad zet, voelt deze veel zachter en natuurlijker aan dan een massieve shad wat het 
-verspelen van vis sterk verminderd.', 9.98, 6, 2); --pulse
+verspelen van vis sterk verminderd.', 9.98, 6, 2);
 
+--08
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('56511', 'Fox Rage Spinnerbait', 'Wanneer snoek niet wilt bijten dan moet je
  deze spinnerbait eens proberen. Deze spinnerbait zorgt voor zo veel beweging en 
  attractie in het water dat snoek er gewoon niet omheen kan. De spinnerbait heeft 
  twee spinnerbladen en een grote bucktail met daarin een haak verstopt. 
- De ogen op de spinnerbait maken de aantrekkingskracht af.', 11.77, 9, 2); --spinner
+ De ogen op de spinnerbait maken de aantrekkingskracht af.', 11.77, 9, 2);
 
+--09
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('12345', 'Effzett Pike Rattlin Spinner', 'De Effzett Pike Spinner is op 
 meerdere manieren onweerstaanbaar. De spinnerbladen zorgen voor een goede 
 waterverplaatsing onderwater, hierdoor zal de snoek het kunstaasje 
 snel opmerken. Daarnaast is de spinner voorzien van ratel, zodat hij nog
 meer opvalt. Vis de spinner met gevarieerde snelheid en de aanbeten zullen 
-niet uitblijven.', 11.67, 8, 2); --spinner
+niet uitblijven.', 11.67, 8, 2);
 
+--10
 INSERT INTO products (code, name, description, price, ook_bekeken, producttype)
 VALUES ('76594', 'Abu Garcia Svartzonker McHybrid Small', 'Mc Hybrid geeft je het 
 beste van 2 werelden. De duurzaamheid van een hard aas en tegelijkertijd de softe 
@@ -81,32 +93,51 @@ actie van een shad tail. Deze plug kan op twee verschillende dieptes ingezet wor
 De lage ring is voor de ondiepere actie en de hogere ring voor de diepere actie. 
 McHybrid kan ook gecombineerd worden met soft tails van McMy en McMio. 
 Dit geeft de plug een langzamere actie, waardoor deze bij lagere watertemperaturen 
-goed te gebruiken is.', 15.75, 7, 2); -- pulse
+goed te gebruiken is.', 15.75, 7, 2);
 
+
+-- hengeleigenschappen tabel
+--01
 INSERT INTO hengel_eigenschappen (lengte, max_gewicht, hengelsoort_id) VALUES (188, 45, 1);
 
+--02
 INSERT INTO hengel_eigenschappen (lengte, max_gewicht, hengelsoort_id) VALUES (195, 45, 1);
 
+--03
 INSERT INTO hengel_eigenschappen (lengte, max_gewicht, hengelsoort_id) VALUES (210, 25, 2);
 
+--04
 INSERT INTO hengel_eigenschappen (lengte, max_gewicht, hengelsoort_id) VALUES (210, 30, 1);
 
+--05
 INSERT INTO hengel_eigenschappen (lengte, max_gewicht, hengelsoort_id) VALUES (274, 60, 2);
 
+--soort hengel tabel
+--01
 INSERT INTO hengel_soort (hengelsoort) VALUES ('hengel met reel');
 
+--02
 INSERT INTO hengel_soort (hengelsoort) VALUES ('hengel met molen');
 
+--soort aas tabel
+--01
 INSERT INTO soort_aas (aassoort) VALUES ('Pulsebait');
 
+--02
 INSERT INTO soort_aas (aassoort) VALUES ('Spinnerbait'); 
 
-INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(21, 7.5);
+-- aaseigenschappen tabel
+--01
+INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(21, 7.5, 1);
 
-INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(15, 10);
+--02
+INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(15, 10, 1);
 
-INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(14, 8.5);
+--03
+INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(14, 8.5, 2);
 
-INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(40, 15.5);
+--04
+INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(40, 15.5, 2);
 
-INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(68, 15.5);
+--05
+INSERT INTO aas_eigenschappen (gewicht, lengte) VALUES(68, 15.5, 2);
